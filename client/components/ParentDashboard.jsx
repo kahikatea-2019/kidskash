5c1d6194794aa1f7eebde4fcb07a57c05f1fd5fb
Eteroa123 <eteroalafaele080@gmail.com>
2019-05-09 15:01:05 +1200

    Added Parent dashboard structure

---
+++ client/components/ParentDashboard.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+class ParentDashboard extends  React.Component{
+ render(){
+     return(
+        <React.Fragment>
+        <h1>Parents Dashboard </h1>
+        </ React.Fragment>
+        )
+     }
+ }
+
+
+
+export default ParentDashboard
