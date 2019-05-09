9579dd0efc03910cce9926cdd84c429c6fe180a9
Philip Ji <phil12p345@gmail.com>
2019-05-09 18:38:13 +1200

    work in progress

---
+++ test/test-environment.js
@@ -0,0 +1,3 @@
+const getDbConn = require('knex')
+
+const testConfig = require('../server/')
--- test/test.test.js
+++ test/test.test.js
@@ -1,1 +1,7 @@
 // All tests should mimic the structure of a the file structure it is mimicing - for example a test for an actions should be in test/actions/actionname.test.js
+
+// git add test/client/components/Welcome.test.js
+// git commit -m "Created Welcome.test.js"
+
+// git add test/setup.js
+// git commit -m "Creating setup.js to configure enzyme adapter"
