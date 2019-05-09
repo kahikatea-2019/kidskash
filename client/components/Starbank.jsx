e84c3b56013e13e525cb31780574e6a82ab712f3
Anandani Mala Prakash <anandaniprakash@icloud.com>
2019-05-09 16:55:05 +1200

    starbank component done

---
+++ client/components/Starbank.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Starbank () {
+  return (
+    <div>
+      <h2>This is where I will see my collection of stars from great behaviour and helping out</h2>
+    </div>
+  )
+}
+
+export default Starbank
