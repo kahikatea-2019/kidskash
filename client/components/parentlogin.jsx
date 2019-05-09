a6a6536550d96a8d46fc3b4373680b48092590ab
Dani <anandaniprakash@icloud.com>
2019-05-09 14:56:18 +1200

    part one

---
+++ client/components/parentlogin.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const parentlogin= () => {
+  return (
+      <div>
+    <h2>Hi Mum or Dad...</h2>
+    <button>Login</button>
+    <button>Register</button>
+
+    </div>
+  )
+}
+
+export default parentlogin
