c567a538da1ebcb315d0276f4ba3fa6ba5def2c0
Anandani Mala Prakash <anandaniprakash@icloud.com>
2019-05-09 16:42:00 +1200

    create Childdashboard

---
+++ client/components/Childdashboard.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Childdashboard () {
+  return (
+    <div>
+      <h2>Child Dashboard</h2>
+      <p> Over here I will be able to add my wishlist and see my starboard </p>
+    </div>
+  )
+}
+
+export default Childdashboard
