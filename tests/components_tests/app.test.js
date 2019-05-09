d80254bfd3880239b1e4548012563a1050b07fe0
Amy Li <amy_tako@hotmail.com>
2019-05-10 11:21:46 +1200

    test app.jsx passed

---
+++ tests/components_tests/app.test.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import { shallow } from 'enzyme'
+import App from '../../client/components/App'
+
+test('test <App/> is set up correctly',() => {
+    expect(true).toBe(true)
+})
+
+test('<App/> has 1 React.Fragment',() => {
+    const wrapper = shallow (<App />)
+    expect(wrapper.find('React.Fragment').length).toBe(0)
+})
+
+
+const app = shallow(<App />);
+
+describe("App", () => {
+  it("renders correctly", () => {
+    expect(app).toMatchSnapshot();
+  });
+});
