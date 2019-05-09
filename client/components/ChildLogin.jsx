dfece96639e956f9a24521b11a457b696966aad6
Eteroa123 <eteroalafaele080@gmail.com>
2019-05-10 11:23:12 +1200

    Made image into a button

--- client/components/ChildLogin.jsx
+++ client/components/ChildLogin.jsx
@@ -4,7 +4,7 @@ function ChildLogin () {
   return (
     <div>
       <h1>Lets Login and have some fun</h1>
-      <img src='/images/child.jpg'/>
+      <button><img src='/images/child.jpg' width="300" height="300"/></button>
     </div>
 
   )
