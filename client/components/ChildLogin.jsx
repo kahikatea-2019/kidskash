dad35595fd6b52096c71bbd131ca8428fd8dbe45
Eteroa123 <eteroalafaele080@gmail.com>
2019-05-10 10:20:06 +1200

    REmoved the src for image

--- client/components/ChildLogin.jsx
+++ client/components/ChildLogin.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 
+
 function ChildLogin () {
   return (
     <div>
       <h1>Lets Login and have some fun</h1>
-      <img src=''/>
+      <img src= "../../server/public/data/child.jpg"/>
     </div>
 
   )
