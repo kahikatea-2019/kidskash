28e1d50a202cb28385724b6b831fd2bf456242f5
Amy Li <amy_tako@hotmail.com>
2019-05-10 14:27:55 +1200

    remove the import helloworld

--- tests/components/app.test.js
+++ tests/components/app.test.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { shallow } from 'enzyme'
 import App from '../../client/components/App'
-import HelloWorld from '../../client/components/HelloWorld'
+
 
 const app = shallow(<App />)
 
