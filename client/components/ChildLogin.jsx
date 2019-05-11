48e4e0fb5a1d0d288c7baa0078483b80687d4031
Eteroa123 <eteroalafaele080@gmail.com>
2019-05-11 22:03:42 +1200

    Images rendering

--- client/components/ChildLogin.jsx
+++ client/components/ChildLogin.jsx
@@ -3,11 +3,14 @@ import React from 'react'
 function ChildLogin () {
   return (
     <div>
+       <center>
       <h1>Lets Login and have some fun</h1>
       <button><img src='/images/babyA.jpg' width="300" height="300"/></button>
       <button><img src='/images/babyB.jpg' width="300" height="300"/></button>
       <button><img src='/images/babyD.jpg' width="300" height="300"/></button>
       <button><img src='/images/babyE.jpg' width="300" height="300"/></button>
+      <button><img src='/images/babyP.jpg' width="300" height="300"/></button>
+      </center>
     </div>
 
   )
