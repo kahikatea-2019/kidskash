ea40a37e58dcca636cc1579883080fc5ab1403ac
Matthew James Bialostocki <matt.bialostocki@gmail.com>
2019-05-11 17:14:42 +1200

    updated wishes.test.js

--- tests/server/db/wishes.test.js
+++ tests/server/db/wishes.test.js
@@ -4,28 +4,28 @@ const db = require('../../../server/db/wishes')
 let testDb = null
 
 beforeEach(() => {
-    testDb = testEnv.getTestDb()
-    return testEnv.initialise(testDb)
+  testDb = testEnv.getTestDb()
+  return testEnv.initialise(testDb)
 })
 
 afterEach(() => testEnv.cleanup(testDb))
 
-test('getWishes get all wishes', () => {
-    const expected = 4
-    return db.getWishes(testDb)
-    .then (wishes => {
-        const actual = wishes.length
-        expect(actual).toBe(expected)
+test('retrieveAllWishes get all wishes', () => {
+  const expected = 4
+  return db.retrieveAllWishes(testDb)
+    .then(wishes => {
+      const actual = wishes.length
+      expect(actual).toBe(expected)
     })
     .catch(err => expect(err).toBeNull())
 })
 
 test('getWish gets a single wish', () => {
-    const expected = 'lollipop'
-    return db.getWish(1, testDb)
-    .then( wish => {
-        const actual = wish.content 
-        expect(actual).toBe(expected)
+  const expected = 'lollipop'
+  return db.getWish(1, testDb)
+    .then(wish => {
+      const actual = wish.content
+      expect(actual).toBe(expected)
     })
     .catch(err => expect(err).toBeNull())
 })
