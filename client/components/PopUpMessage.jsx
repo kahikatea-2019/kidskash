bfd086fefeed328b71afb1345283717538f98a19
Philip Ji <phil12p345@gmail.com>
2019-05-11 17:11:07 +1200

    welcome showing

--- client/components/PopUpMessage.jsx
+++ client/components/PopUpMessage.jsx
@@ -4,26 +4,24 @@ const divStyle = {
   background: 'pink',
   textAlign: 'center',
   padding: '100px 0px 100px 0px',
-  width: '200px'
+  width: '200px',
+  borderRadius: '4px',
+  fontSize: '40px'
 }
 
 class PopUpMessage extends React.Component {
-  constructor () {
-    super()
-    this.state = {
+    state = {
       showHeader: false
     }
-  }
+    componentDidMount () {
+      setTimeout(() => this.setState({ showHeader: true }), 2000)
+    }
 
-  componentDidMount () {
-    setTimeout(() => this.setState({ showHeader: true }), 2000)
-  }
-
-  render () {
-    return (
-      this.state.showHeader && <div style={divStyle}>Welcome!</div>
-    )
-  }
+    render () {
+      return (
+        this.state.showHeader && <div style={divStyle}>Welcome!</div>
+      )
+    }
 }
 
 export default PopUpMessage
