8424c20ed7ea926c54b483105b9a0430df89ae2c
Eteroa123 <eteroalafaele080@gmail.com>
2019-05-11 19:12:41 +1200

    Added the images of babys in the img src

--- client/components/ChildLogin.jsx
+++ client/components/ChildLogin.jsx
@@ -4,7 +4,10 @@ function ChildLogin () {
   return (
     <div>
       <h1>Lets Login and have some fun</h1>
-      <button><img src='/images/child.jpg' width="300" height="300"/></button>
+      <button><img src='/images/babyA.jpg' width="300" height="300"/></button>
+      <button><img src='/images/babyB.jpg' width="300" height="300"/></button>
+      <button><img src='/images/babyD.jpg' width="300" height="300"/></button>
+      <button><img src='/images/babyE.jpg' width="300" height="300"/></button>
     </div>
 
   )
