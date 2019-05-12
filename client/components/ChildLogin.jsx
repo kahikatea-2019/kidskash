a6cc0f920871a6612dfa1049cbedc7c1c40873a0
Matthew James Bialostocki <matt.bialostocki@gmail.com>
2019-05-12 13:32:51 +1200

    ChildLogin returns user data

--- client/components/ChildLogin.jsx
+++ client/components/ChildLogin.jsx
@@ -1,19 +1,32 @@
 import React from 'react'
+import { connect } from 'react-redux'
 
-function ChildLogin () {
+import ChildLoginEntry from './ChildLoginEntry'
+
+function ChildLogin (props) {
+  const { children } = props
+  const loginArray = children.map(child => {
+    return <ChildLoginEntry key={child.id} child={child} />
+  })
+
+  console.log(`children `, children)
+  console.log('loginArray ', loginArray)
+
   return (
-    <div>
-       <center>
-      <h1>KIDS - KA$H</h1>
-      <button><img src='/images/babyA.jpg' width="300" height="300"/></button>
-      <button><img src='/images/babyB.jpg' width="300" height="300"/></button>
-      <button><img src='/images/babyD.jpg' width="300" height="300"/></button>
-      <button><img src='/images/babyE.jpg' width="300" height="300"/></button>
-      <button><img src='/images/babyP.jpg' width="300" height="300"/></button>
+    <React.Fragment>
+      <center>
+        <h1>KIDS - KA$H</h1>
+        {loginArray}
       </center>
-    </div>
+    </React.Fragment>
 
   )
 }
 
-export default ChildLogin
+function mapStateToProps (state) {
+  return {
+    children: state.retrieveAllChildren
+  }
+}
+
+export default connect(mapStateToProps)(ChildLogin)
