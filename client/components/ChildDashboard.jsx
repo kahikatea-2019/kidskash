0af5031555aa862c330468ee45a3d2585fe43f8c
Matthew James Bialostocki <matt.bialostocki@gmail.com>
2019-05-12 15:34:01 +1200

    All structures are drafted

--- client/components/ChildDashboard.jsx
+++ client/components/ChildDashboard.jsx
@@ -1,21 +1,38 @@
 import React from 'react'
+// import { connect } from 'react-redux'
 
 import WishList from './WishList'
 import StarBank from './StarBank'
 
-function ChildDashboard () {
+export default function ChildDashboard () {
+  const child = {
+    id: 1,
+    child_name: 'Amy',
+    display_url: 'https://i.pinimg.com/736x/62/c5/c0/62c5c004441b4bd92e9228198aa17aeb.jpg',
+    parents_id: '1'
+  }
+  // const {dispatch, wishes} = props
+  // function returnChildWishes (wishes) {
+  //   const childWishes = wishes.forEach(wish => {
+  //     if (wish.child_id === child.id) {
+  //       return wish
+  //     }
+  //     return childWishes
+  //   })
+  // }
+
   return (
     <React.Fragment>
       <div className="cd-body">
         <div className="high-frame">
           <div className="welcome-user">
-            {`Hello, Ete!`}
+            {`Hello, ${child.child_name}!`}
           </div>
           <input></input>
         </div>
         <div className="low-frame">
-          <WishList />
-          <StarBank />
+          <WishList id={child.id}/>
+          <StarBank id={child.id}/>
         </div>
 
       </div>
@@ -23,4 +40,10 @@ function ChildDashboard () {
   )
 }
 
-export default ChildDashboard
+// function mapStateToProps (state) {
+//   return {
+//     wishes: state.retrieveAllWishes
+//   }
+// }
+
+// export default connect(mapStateToProps)(ChildDashboard)
--- client/components/ChildLoginEntry.jsx
+++ client/components/ChildLoginEntry.jsx
@@ -1,8 +1,6 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 
-// import ChildDashboard from './ChildDashboard'
-
 function ChildLoginEntry (props) {
   const { id, child_name, display_URL } = props.child
   return (
