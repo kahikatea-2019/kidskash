94c8838b3936187ea8da610483a829539cc27e8a
Philip Ji <phil12p345@gmail.com>
2019-05-12 16:18:05 +1200

    add reducer test

--- tests/client/reducers/retriveAllWishes.test.js
+++ tests/client/reducers/retriveAllWishes.test.js
@@ -2,6 +2,29 @@ import retriveAllWishes from '../../../client/reducers/retrieveAllWishes'
 
 test('reducer populates some initial state', () => {
   const newState = retriveAllWishes(undefined, '@@INIT')
-  expect(Array.isArray(newState.wishes)).toBeTruthy()
+  expect(Array(newState.wishes)).toBeTruthy()
 })
 
+test('reducer handles RECEIVE_WISHES correctly', () => {
+  const currentState = [{ 0: 0 }, { 1: 1 }, { 2: 2 }]
+
+  const action = {
+    type: 'UNKNOWN_ACTION'
+  }
+  const newState = retriveAllWishes(currentState,
+    action)
+  expect(newState).toBe(currentState)
+})
+
+test('reducer returns current state when action does not match', () => {
+  const currentState = [{ 0: 0 }, { 1: 1 }, { 2: 2 }]
+
+  const action = {
+    type: 'RECEIVE_WISHES',
+    wishes: currentState
+  }
+  const newState = retriveAllWishes(currentState, action)
+
+  expect(newState.length).toBe(3)
+  expect(newState).toBe(currentState)
+})
