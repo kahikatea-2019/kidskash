290e13d9eaccc96e9272a5978d86396a77543346
Philip Ji <phil12p345@gmail.com>
2019-05-13 12:55:39 +1200

    Merge branch 'dev' of https://github.com/kahikatea-2019/kidskash into 77-GET-route-starbank

---
+++ tests/client/reducers/retrieveAllWishes.test.js
@@ -0,0 +1,30 @@
+import retrieveAllWishes from '../../../client/reducers/retrieveAllWishes'
+
+test('reducer populates some initial state', () => {
+  const newState = retrieveAllWishes(undefined, '@@INIT')
+  expect(Array(newState.wishes)).toBeTruthy()
+})
+
+test('reducer handles RECEIVE_WISHES correctly', () => {
+  const currentState = [{ 0: 0 }, { 1: 1 }, { 2: 2 }]
+
+  const action = {
+    type: 'UNKNOWN_ACTION'
+  }
+  const newState = retrieveAllWishes(currentState,
+    action)
+  expect(newState).toBe(currentState)
+})
+
+test('reducer returns current state when action does not match', () => {
+  const currentState = [{ 0: 0 }, { 1: 1 }, { 2: 2 }]
+
+  const action = {
+    type: 'RECEIVE_WISHES',
+    wishes: currentState
+  }
+  const newState = retrieveAllWishes(currentState, action)
+
+  expect(newState.length).toBe(3)
+  expect(newState).toBe(currentState)
+})
