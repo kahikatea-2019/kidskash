e1b248d95e190895f969d22029a54602163f64ba
Matthew James Bialostocki <matt.bialostocki@gmail.com>
2019-05-14 10:21:30 +1200

    dev-alignment && code beautify

--- client/components/WishInput.jsx
+++ client/components/WishInput.jsx
@@ -4,7 +4,6 @@ import { actions, Control, Errors, Form } from 'react-redux-form'
 
 import { addWish } from '../actions/wishes'
 
-
 class WishInput extends React.Component {
   constructor (props) {
     super(props)
@@ -12,11 +11,11 @@ class WishInput extends React.Component {
     this.handleSubmit = this.handleSubmit.bind(this)
   }
 
-//   handleChange(e){
-//     this.setState({
-//       content:e.target.value
-//     })
-//   }
+  //   handleChange(e){
+  //     this.setState({
+  //       content:e.target.value
+  //     })
+  //   }
 
   handleSubmit (wish) {
     const { dispatch } = this.props
@@ -26,22 +25,22 @@ class WishInput extends React.Component {
 
   render () {
     return (
-        <div className= 'wishinput'>
+      <div className= 'wishinput'>
         <h2>Mum/Dad: These are what i want :</h2>
-      <Form model='content' onSubmit={this.handleSubmit}>
-        <label>Wish:</label>
-        <Control.text model='.wish'
-          className='u-full-width' validateOn='blur'
-          validators={{ isRequired: content => content && content.length }}
-        />
-        <Errors model=".wish" className="error" show='touched'
-          messages={{ isRequired: 'Please write  a wish.' }}
-        />
-        {/* this could be another componment for each star container */}
-        <label>Stars:</label>
-        <Control.select model='.stars' className='u-full-width' />
-        <button type='submit' className='button-primary'>Add</button>
-      </Form>
+        <Form model='content' onSubmit={this.handleSubmit}>
+          <label>Wish:</label>
+          <Control.text model='.wish'
+            className='u-full-width' validateOn='blur'
+            validators={{ isRequired: content => content && content.length }}
+          />
+          <Errors model=".wish" className="error" show='touched'
+            messages={{ isRequired: 'Please write  a wish.' }}
+          />
+          {/* this could be another componment for each star container */}
+          <label>Stars:</label>
+          <Control.select model='.stars' className='u-full-width' />
+          <button type='submit' className='button-primary'>Add</button>
+        </Form>
       </div>
     )
   }
--- client/components/WishList.jsx
+++ client/components/WishList.jsx
@@ -2,44 +2,41 @@ import React from 'react'
 import { connect } from 'react-redux'
 import * as wishesAction from '../actions/wishes'
 
+class WishList extends React.Component {
+  deleteWish (e, id) {
+    e.preventDefault()
+    this.props.deleteWish(id)
+  }
 
-class WishList extends React.Component  {
- 
-    deleteWish (e,id) {
-      e.preventDefault()
-      this.props.deleteWish(id)
-    }
-
-    receiveWish(wish) {
-      const {id,content,stars} = wish
-      return(
-        <tr key={id} className='wish' onClick={() => this.props.deleteWish(id)}>
+  receiveWish (wish) {
+    const { id, content, stars } = wish
+    return (
+      <tr key={id} className='wish' onClick={() => this.deleteWish(id)}>
         <td className="item-content">{content}</td>
         <td className="stars" style={{ backgroundImage: '' }}>{stars}</td>
-        </tr>
-      )
-    }
- 
+      </tr>
+    )
+  }
 
-  render() {
-  return (
-    <div className='wishllist'>
-      <h2>Mum/Dad: These are what i want :</h2>
+  render () {
+    return (
+      <div className='wishllist'>
+        <h2>Wishlist</h2>
         <hr/>
-    <table className="u-full-width">
-      <thead>
-        <tr>
-          <th className="item-content">Wishes</th>
-          <th className="stars">Stars</th>
-        </tr>
-      </thead>
-      <tbody>
-        {this.props.wish.map(wish => this.receiveWish(wish))}
-      </tbody>
-    </table>
-    </div>
-  )
-}
+        <table className="u-full-width">
+          <thead>
+            <tr>
+              <th className="item-content">Wishes</th>
+              <th className="stars">Stars</th>
+            </tr>
+          </thead>
+          <tbody>
+            {this.props.wish.map(wish => this.receiveWish(wish))}
+          </tbody>
+        </table>
+      </div>
+    )
+  }
 }
 
 const mapStateToProps = ({ wish }) => {
@@ -56,4 +53,3 @@ const mapDispatchToProps = dispatch => {
 }
 
 export default connect(mapStateToProps, mapDispatchToProps)(WishList)
-
