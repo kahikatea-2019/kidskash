4eb60e73d24227635bb4238d4610d75b4868db93
Matthew James Bialostocki <matt.bialostocki@gmail.com>
2019-05-13 16:29:37 +1200

    Phantom comment refactored

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -7,14 +7,12 @@ import AppRoutes from './AppRoutes'
 // action calls
 import { retrieveAllChildren } from '../actions/children'
 import { retrieveAllWishes } from '../actions/wishes'
-// import { retrieveAllStarBanks } from '../actions/starbanks'
 // awaiting starbanks get route && db function
 
 function App (props) {
   const { dispatch } = props
   dispatch(retrieveAllChildren())
   dispatch(retrieveAllWishes())
-  // dispatch(retrieveAllStarBanks)
   return (
     <React.Fragment>
       <AppRoutes/>
