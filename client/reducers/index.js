a2b692561579a1f8dcede25f24ce0c9f7dc9586d
Matthew James Bialostocki <matt.bialostocki@gmail.com>
2019-05-13 17:28:06 +1200

    starbanks Action && Reducer set

--- client/reducers/index.js
+++ client/reducers/index.js
@@ -2,11 +2,12 @@ import { combineReducers } from 'redux'
 
 import retrieveAllChildren from './retrieveAllChildren'
 import retrieveAllWishes from './retrieveAllWishes'
+import retrieveAllStarBanks from './retrieveAllStarBanks'
 import navigate from './navigate'
 
 export default combineReducers({
   retrieveAllChildren,
   retrieveAllWishes,
+  retrieveAllStarBanks,
   navigate
-
 })
