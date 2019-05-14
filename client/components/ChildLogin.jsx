e97306d18e7f86975b8a85c075f18adca9dad4e6
Anandani Mala Prakash <anandaniprakash@icloud.com>
2019-05-14 17:12:42 +1200

    homepage design done

--- client/components/ChildLogin.jsx
+++ client/components/ChildLogin.jsx
@@ -2,24 +2,41 @@ import React from 'react'
 import { connect } from 'react-redux'
 import MaterialUI from './MaterialUi'
 
-import ChildLoginEntry from './ChildLoginEntry'
+import Typography from '@material-ui/core/Typography'
+import Grid from '@material-ui/core/Grid'
 
 function ChildLogin (props) {
   const { children } = props
-  const loginArray = children.map(child => {
-    return <ChildLoginEntry key={child.id} child={child} />
-  })
 
-  console.log(`children `, children)
-  console.log('loginArray ', loginArray)
+  const loginArray = children.map(child => {
+    return <MaterialUI key={child.id} child={child} />
+  })
 
   return (
     <React.Fragment>
       <center>
         <h1>KIDS - KA$H</h1>
-        <div className="login-frame">{loginArray}</div>
+        <div className='heroUnit'>
+          <div className='heroContent'>
+            <Typography component="h1" variant="h2" align="center" color="textPrimary" gutterBottom>
+                Kiddiez Log In
+            </Typography>
+            <Typography variant="h6" align="center" color="textSecondary" paragraph>
+              Click on your picture!!
+            </Typography>
+          </div>
+        </div>
+        <Grid
+          container='true'
+          justify='center' >
+          <main>
+            <div className=' layout, cardGrid'>
+              <Grid container spacing={40}>{loginArray}</Grid>
+            </div>
+          </main>
+        </Grid>
+
       </center>
-      <MaterialUI/>
     </React.Fragment>
 
   )
--- client/components/MaterialUi.jsx
+++ client/components/MaterialUi.jsx
@@ -1,7 +1,12 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import classNames from 'classnames'
-import AppBar from '@material-ui/core/AppBar'
+
+import { Link } from 'react-router-dom'
+import { connect } from 'react-redux'
+
+// Action Imports
+import { navigate, currentUser } from '../actions/navigate'
+
 import Button from '@material-ui/core/Button'
 import Card from '@material-ui/core/Card'
 import CardActions from '@material-ui/core/CardActions'
@@ -9,7 +14,6 @@ import CardContent from '@material-ui/core/CardContent'
 import CardMedia from '@material-ui/core/CardMedia'
 import CssBaseline from '@material-ui/core/CssBaseline'
 import Grid from '@material-ui/core/Grid'
-import Toolbar from '@material-ui/core/Toolbar'
 import Typography from '@material-ui/core/Typography'
 import { withStyles } from '@material-ui/core/styles'
 import { yellow } from '@material-ui/core/colors'
@@ -63,74 +67,51 @@ const styles = theme => ({
   }
 })
 
-const cards = [1, 2, 3, 4, 5, 6, 7, 8]
+const Album = props => {
+  const { child, dispatch, classes } = props
+  const { id, child_name } = child
+  const userId = id
 
-function Album (props) {
-  const { classes } = props
+  function trigger () {
+    dispatch(navigate('dashboard'))
+    dispatch(currentUser(userId))
+  }
 
   return (
     <React.Fragment>
       <CssBaseline />
-      <AppBar position="static" className={classes.appBar}>
-        <Toolbar>
-          <Typography variant="h6" color="yellow" noWrap>
-            Kids Ka$h
-          </Typography>
-        </Toolbar>
-      </AppBar>
-      <main>
-        {/* Hero unit */}
-        <div className={classes.heroUnit}>
-          <div className={classes.heroContent}>
-            <Typography component="h1" variant="h2" align="center" color="textPrimary" gutterBottom>
-                Kiddiez Log In
-            </Typography>
-            <Typography variant="h6" align="center" color="textSecondary" paragraph>
-              Click on your picture!!
-            </Typography>
-          </div>
-        </div>
-        <div className={classNames(classes.layout, classes.cardGrid)}>
-          {/* End hero unit */}
-          <Grid container spacing={40}>
-            {cards.map(card => (
-              <Grid item key={card} sm={6} md={4} lg={3}>
-                <Card className={classes.card}>
-                  <CardMedia
 
-                    className={classes.cardMedia}
-                    image= "https://previews.123rf.com/images/sergt/sergt1606/sergt160600068/57936472-giraffe-vector-icon-giraffe-flat-icon-round-icon-giraffe-head-icon.jpg"
-                    title="Hey its me!"
-                  />
-                  <CardContent className={classes.cardContent}>
-                    <Typography gutterBottom variant="h5" component="h2">
-                      Munchkin
-                    </Typography>
-                    <Typography>
+      <Grid item sm={6} md={4} lg={3}>
+
+        <Card className={classes.card}>
+          <CardMedia
+            className={classes.cardMedia}
+            image= "https://previews.123rf.com/images/sergt/sergt1606/sergt160600068/57936472-giraffe-vector-icon-giraffe-flat-icon-round-icon-giraffe-head-icon.jpg"
+            title="Hey its me!"
+          />
+          <CardContent className={classes.cardContent}>
+            <Typography gutterBottom variant="h5" component="h2">
+              {child_name}
+            </Typography>
+            <Typography>
                       Click here to view your Wishlist
-                    </Typography>
-                  </CardContent>
-                  <CardActions>
-                    <Button size="small" color="primary" style={{ justifyContent: 'center' }}>
-                    View
-                    </Button>
-                  </CardActions>
-                </Card>
-              </Grid>
-            ))}
-          </Grid>
-        </div>
-      </main>
-      {/* Footer */}
-      <footer className={classes.footer}>
-        <Typography variant="h6" align="center" gutterBottom>
-          Copywrite Kids Ka$h
-        </Typography>
-        <Typography variant="subtitle1" align="center" color="textSecondary" component="p">
-          Teach your kids to be a better version of themselves.. and you...
-        </Typography>
-      </footer>
-      {/* End footer */}
+            </Typography>
+          </CardContent>
+          <CardActions>
+            <Button
+              size="small"
+              variant="contained"
+              color="default"
+              style={{ justifyContent: 'center' }}>
+              <Link to="#" onClick={trigger}>
+                Log-in
+              </Link>
+            </Button>
+          </CardActions>
+        </Card>
+
+      </Grid>
+
     </React.Fragment>
   )
 }
@@ -139,4 +120,10 @@ Album.propTypes = {
   classes: PropTypes.object.isRequired
 }
 
-export default withStyles(styles)(Album)
+function mapStateToProps (state) {
+  return {
+    currentUser: state.navigate.currentUser
+  }
+}
+
+export default connect(mapStateToProps)(withStyles(styles)(Album))
