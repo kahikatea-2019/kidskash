c1208bfbc87cb85bb4d2da882b04199cc9c60102
Amy Li <amy_tako@hotmail.com>
2019-05-15 18:12:31 +1200

    display stars in starbanks structure

--- client/components/StarBank.jsx
+++ client/components/StarBank.jsx
@@ -7,19 +7,30 @@ class StarBank extends React.Component {
   }
 
   render () {
+    const { boxed_stars,child_id} = this.props.starbanks
+    const { starbanks, currentUser } = this.props
+    const currentStarBank = starbanks.find(starbank => {
+      return (starbank.child_id === currentUser)
+    })
+   const thisStarBox = currentStarBank.boxed_stars
+
     return (
       <React.Fragment>
-        <div>
-          <h2>This is where I will see my collection of stars from great behaviour and helping out</h2>
-          <div className="allocated-stars">stars2</div>
+        <div className="starbox">
+
+          <div className="starbox">{thisStarBox}</div>
+         
         </div>
       </React.Fragment>
     )
   }
 }
+
+
 function mapStateToProps (state) {
   return {
-
+    starbanks: state.starbanks,
+    currentUser: state.navigate.currentUser
   }
 }
 
