ea50870b972c3b9aed3d28704c9e7db4840f94ae
Matthew James Bialostocki <matt.bialostocki@gmail.com>
2019-05-15 18:22:23 +1200

    Render StarBank in ChildDashboard

--- client/components/StarBank.jsx
+++ client/components/StarBank.jsx
@@ -2,30 +2,25 @@ import React from 'react'
 import { connect } from 'react-redux'
 
 class StarBank extends React.Component {
-  constructor (props) {
-    super(props)
-  }
-
   render () {
-    const { boxed_stars,child_id} = this.props.starbanks
     const { starbanks, currentUser } = this.props
     const currentStarBank = starbanks.find(starbank => {
       return (starbank.child_id === currentUser)
     })
-   const thisStarBox = currentStarBank.boxed_stars
+    const thisStarBox = currentStarBank.boxed_stars
 
     return (
       <React.Fragment>
         <div className="starbox">
 
-          <div className="starbox">{thisStarBox}</div>
-         
+          <h2>My StarBox</h2>
+          <div className="boxed-stars">{thisStarBox}</div>
+
         </div>
       </React.Fragment>
     )
   }
 }
-
 
 function mapStateToProps (state) {
   return {
