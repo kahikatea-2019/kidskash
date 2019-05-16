d3e9d041d4a31f5d1fa802e2262d07eb0b0d874e
Chris Laupama <chrislaupama@icloud.com>
2019-05-16 14:48:27 +1200

    slight changes to design

--- client/components/Wish.jsx
+++ client/components/Wish.jsx
@@ -1,12 +1,18 @@
-import React from 'react'
-import { connect } from 'react-redux'
+import React from "react"
+import { connect } from "react-redux"
 
 // import actions
-import { updateStars } from '../actions/wishes'
-import { updateStarBanks } from '../actions/starbanks'
+import { updateStars } from "../actions/wishes"
+import { updateStarBanks } from "../actions/starbanks"
 
 class Wish extends React.Component {
-  incrementClick = (wishId, childId, allocatedStars, boxedStars, required_stars) => {
+  incrementClick = (
+    wishId,
+    childId,
+    allocatedStars,
+    boxedStars,
+    required_stars
+  ) => {
     const { dispatch } = this.props
     const newAllocated = allocatedStars + 1
     const newBoxed = boxedStars - 1
@@ -28,25 +34,67 @@ class Wish extends React.Component {
     }
   }
 
-  render () {
-    const { id, content, required_stars, allocated_stars, child_id } = this.props.wish
+  render() {
+    const {
+      id,
+      content,
+      required_stars,
+      allocated_stars,
+      child_id
+    } = this.props.wish
     const { starbanks, currentUser } = this.props
     const currentStarBank = starbanks.find(box => {
-      return (box.child_id === currentUser)
+      return box.child_id === currentUser
     })
     const boxedStars = currentStarBank.boxed_stars
 
     return (
       <React.Fragment>
-        <div className="individual-wish-content">
-          <div className="wish-data-wrapper">
-            <div className="wish"><p>{content}</p></div>
-            <div className="required-stars"><p>{required_stars}</p></div>
-            <div className="allocated-stars"><p>{allocated_stars}</p></div>
-          </div>
-          <div className="button-wrapper">
-            <button onClick ={() => this.incrementClick(id, child_id, allocated_stars, boxedStars, required_stars)} className="increment-button">+</button>
-            <button onClick ={() => this.decrementClick(id, child_id, allocated_stars, boxedStars)} className="decrement-button">-</button>
+        <div className='individual-wish-content'>
+          <table>
+            <tr>
+              <div>
+                <td>
+                  <div className='wish'>
+                    <p>{content}</p>
+                  </div>
+                </td>
+                <td>
+                  <div className='required-stars'>
+                    <p>{required_stars}</p>
+                  </div>
+                </td>
+                <td>
+                  <div className='allocated-stars'>
+                    <p>{allocated_stars}</p>
+                  </div>
+                </td>
+              </div>
+            </tr>
+          </table>
+          <div className='button-wrapper'>
+            <button
+              onClick={() =>
+                this.incrementClick(
+                  id,
+                  child_id,
+                  allocated_stars,
+                  boxedStars,
+                  required_stars
+                )
+              }
+              className='increment-button'
+            >
+              +
+            </button>
+            <button
+              onClick={() =>
+                this.decrementClick(id, child_id, allocated_stars, boxedStars)
+              }
+              className='decrement-button'
+            >
+              -
+            </button>
           </div>
         </div>
       </React.Fragment>
@@ -54,7 +102,7 @@ class Wish extends React.Component {
   }
 }
 
-function mapStateToProps (state) {
+function mapStateToProps(state) {
   return {
     starbanks: state.starbanks,
     currentUser: state.navigate.currentUser
--- client/components/WishInput.jsx
+++ client/components/WishInput.jsx
@@ -1,49 +1,61 @@
-import React from 'react'
-import { connect } from 'react-redux'
-import { actions, Control, Errors, Form } from 'react-redux-form'
-import { addNewWish } from '../actions/wishes'
+import React from "react"
+import { connect } from "react-redux"
+import { actions, Control, Errors, Form } from "react-redux-form"
+import { addNewWish } from "../actions/wishes"
 
 class WishInput extends React.Component {
-  constructor (props) {
+  constructor(props) {
     super(props)
     this.handleSubmit = this.handleSubmit.bind(this)
   }
 
-  handleSubmit (wish) {
+  handleSubmit(wish) {
     const { dispatch, currentUser } = this.props
 
-    dispatch(addNewWish({
-      ...wish,
-      allocated_stars: 0,
-      child_id: currentUser
-    }))
-    dispatch(actions.reset('wish'))
+    dispatch(
+      addNewWish({
+        ...wish,
+        allocated_stars: 0,
+        child_id: currentUser
+      })
+    )
+    dispatch(actions.reset("wish"))
   }
 
-  render () {
+  render() {
     return (
       // <Card className(props)>
-      <div className= 'wishinput'>
+      <div className='wishinput'>
         {/* <h2>Mum/Dad: These are what i want :</h2> */}
         <Form model='wish' onSubmit={this.handleSubmit}>
-          <label><h2>What will I work towards?</h2></label>
-          <Control.text model='.content'
-            className='u-full-width form-input' validateOn='blur'
+          <label>
+            <h2>What will I work towards?</h2>
+          </label>
+          <Control.text
+            model='.content'
+            className='u-full-width form-input'
+            validateOn='blur'
             validators={{ isRequired: content => content && content.length }}
           />
-          <Errors model=".content" className="error" show='touched'
-            messages={{ isRequired: 'Please write  a wish.' }}
+          <Errors
+            model='.content'
+            className='error'
+            show='touched'
+            messages={{ isRequired: "Please write  a wish." }}
           />
           {/* this could be another componment for each star container */}
-          <label><h2>How much is it worth to me?</h2></label>
-          <Control.select model='.required_stars' className='u-full-width'>
-            <option value="5">5</option>
-            <option value="10">10</option>
-            <option value="15">15</option>
-            <option value="20">20</option>
+          <label>
+            <h2>How much is it worth to me?</h2>
+          </label>
+          <Control.select model='.required_stars' className='u-full-width control'>
+            <option value='5'>5</option>
+            <option value='10'>10</option>
+            <option value='15'>15</option>
+            <option value='20'>20</option>
           </Control.select>
-          <button type='submit' className='button-primary'>Add</button>
-
+          <button type='submit' className='wish-button button-primary'>
+            Add
+          </button>
         </Form>
       </div>
       // </Card>
@@ -51,7 +63,7 @@ class WishInput extends React.Component {
   }
 }
 
-function mapStateToProps (state) {
+function mapStateToProps(state) {
   return {
     currentUser: state.navigate.currentUser
   }
